a4b4e2fea2e8b73a71a50f31ed0fd33a89b6fa0c
kaolugo <kaolugo@umich.edu>
2024-06-17 14:35:32 -0500

    Established backward clouds

--- cloudSimulation.js
+++ cloudSimulation.js
@@ -103,7 +103,7 @@ class ParticleSystem {
 }
 
 class Cloud {
-    constructor(canvas, context, cloudPath, horizontalSpeed, initialPos) {
+    constructor(canvas, context, cloudPath, horizontalSpeed, initialPos, direction) {
         this.canvas = canvas;
         this.ctx = context;
 
@@ -114,10 +114,21 @@ class Cloud {
         this.animate = this.animate.bind(this);
         this.isMoving = false;
 
+        this.initialPos = initialPos;
+
         this.cloudX = 0;
         this.cloudY = 0;
-        this.speedX = horizontalSpeed;
         this.speedY = 0;
+        this.speedX = horizontalSpeed;
+
+        this.direction = direction;
+
+        if (direction === "forward") {
+            this.speedX = horizontalSpeed;
+        } else {
+            this.speedX = -horizontalSpeed;
+        }
+        
     }
 
     animate() {
@@ -139,8 +150,20 @@ class Cloud {
         this.cloudY += this.speedY;
 
         // TODO: adjust restart position here
-        if (this.cloudX > this.canvas.width) this.cloudX = 0;
-        if (this.cloudY > this.canvas.height) this.cloudY = 0;
+        // if (this.cloudX > this.canvas.width) this.cloudX = 0;
+        //     if (this.cloudY > this.canvas.height) this.cloudY = 0;
+        if (this.direction === "forward") {
+            if (this.cloudX + this.initialPos.x > this.canvas.width) this.cloudX = 0;
+        } else {
+            console.log("backward direction");
+            if (this.cloudX < -this.canvas.width) {
+                this.cloudX = this.initialPos.x;
+            }
+        }
+        
+
+
+
         // TODO: add operations here if removing clouds
 
         requestAnimationFrame(this.animate);
@@ -171,7 +194,7 @@ class ResponsiveCanvas {
         this.offscreenCanvas.height = this.canvas.height;
 
         const sampleCloudPath = "m134.52,149.82s2.05-103.64,80.04-103.64,90.52,35.92,91.44,82.09c0,0,148.9-63.44,169.42,50.46,0,0,64.43-5.31,65.46,38.81s-77.99,53.36-77.99,53.36c0,0-54.39,72.86-151.87,33.86,0,0-75.93,102.61-137.5-16.42,0,0-139.56,18.47-147.77-44.12s32.84-87.22,108.77-94.41Z"
-        this.newCloud = new Cloud(this.canvas, this.ctx, sampleCloudPath, 2, {x: -500, y: 100});
+        this.newCloud = new Cloud(this.canvas, this.ctx, sampleCloudPath, 2, {x: 600, y: 100}, "backward");
 
         window.addEventListener('resize', () => this.resizeCanvas());
     }
@@ -202,3 +225,5 @@ responsiveCanvas.newCloud.startAnimation();
 // todo: add other types of clouds
 
 // todo: make clouds come from other side
+
+// create a way to swap clouds based on certain conditions
